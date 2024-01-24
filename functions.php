84e50537a40cd73236e2dc96e41983ac11718bf9
VeljkoBogdan <action@dr.com>
2024-01-24 18:15:29 +0100

    setup

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+
+$GLOBALS['pdo'] = connectDatabase($dsn, $pdoOptions);
+
+/** Function connects to database using PDO
+ * @param string $dsn
+ * @param array $pdoOptions
+ * @return PDO
+ */
+function connectDatabase(string $dsn, array $pdoOptions): PDO
+{
+
+    try {
+        $pdo = new PDO($dsn, PARAMS['USER'], PARAMS['PASSWORD'], $pdoOptions);
+    } catch (\PDOException $e) {
+        var_dump($e->getCode());
+        throw new \PDOException($e->getMessage());
+    }
+
+    return $pdo;
+}
