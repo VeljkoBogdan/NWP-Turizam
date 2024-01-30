2a6aec5732102ef3a7660d5e2f6ad225ee3765e7
VeljkoBogdan <vtsveljkobogdan@gmail.com>
2024-01-30 09:12:29 +0100

    finish

--- leaflet.js
+++ leaflet.js
@@ -1,8 +1,8 @@
-var map = L.map('map').setView([0, 0], 2);
+    var map = L.map('map').setView([0, 0], 2);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
 
 var routingControl;
 var markers = [];
